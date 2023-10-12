0ebc1981b91810c180c083850adb5519dac9f6e1
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-10-12 00:20:30 -0300

    Update homeController.ts

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -5,3 +5,18 @@ export const home = async (req:Request, res: Response)=>{
   const notes = await Note.find({})
   res.render('pages/home', {notes})
 }
+
+export const novaNota = async (req:Request, res: Response) => {
+  const {title, content} = req.body
+  const note = new Note();
+  note.title=title; 
+  note.content=content; 
+  note.usefull=true;
+  try{
+    note.save();
+    console.log('Nota Salva')
+    res.redirect('/')
+  }catch(err){
+    console.log('Error: '+err)
+  }
+}
