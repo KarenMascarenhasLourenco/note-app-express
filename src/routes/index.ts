8fdf4050e8812ba75722cbf40c85142f1b34d9b2
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-10-12 00:20:33 -0300

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,5 +5,6 @@ import * as HomeController from '../controllers/homeController'
 const router = Router();
 
 router.get('/', HomeController.home)
+router.post('/nova-nota', HomeController.novaNota)
 
 export default router;
